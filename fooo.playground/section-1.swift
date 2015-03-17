b797cfc0e023bcf70d98267327441d43d8866875
Martin Siegfried Däbritz <siggi@Martins-MacBook-Pro.local>
2015-03-17 18:13:46 +0100

    verbesserter "Norman"

--- fooo.playground/section-1.swift
+++ fooo.playground/section-1.swift
@@ -21,9 +21,10 @@ class Information {
 class Norman {
 
     var informationen: [Information]
-    
+    var summe: Double
     init (information: [Information]) {
         self.informationen = information
+        summe = 0.0
     }
     
     func addInfo(information: Information) {
@@ -37,22 +38,43 @@ class Norman {
             println(information.inhalt + "\n===========\n\(wissen)")
         }
     }
+    
+    func wissenKumuliert() -> Double {
+        var wissen = 0.0
+        for information in informationen {
+            wissen = wissen + information.nutzen
+        }
+        return wissen
+    }
 }
 
 
 let info = Information(inhalt: "Wiki", nutzen: 0.1)
 let info2 = Information(inhalt: "LateX", nutzen: 0.3)
 let infosammlung = [info,info2]
+
 let norm = Norman(information: infosammlung)
 
 
-norm.printNorman()
+//norm.printNorman()
 
 let info3 = Information(inhalt: "nutzlos", nutzen: 0.0)
 
+let info4 = Information(inhalt: "nerviger Inhalt", nutzen: 0.5)
+
 norm.addInfo(info3)
+norm.addInfo(info4)
 
 norm.printNorman()
+
+println("\nWissen kumuliert: \(norm.wissenKumuliert())")
+
+
+
+
+
+
+
 
 
 
